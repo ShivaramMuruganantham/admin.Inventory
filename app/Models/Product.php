a5276054d5c2ce04202d1ccb69707adc04dd510b
Shivaram <shivaram1216@gmail.com>
2025-07-02 10:13:38 +0530

    Creating product controller and using eloquent model

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+
+    protected $guarded = [];
+
+    public function shop() {
+        return $this->belongsTo(Shop::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function saleItems() {
+        return $this->hasMany(Sale_item::class);
+    }
+
+    public function inventory() {
+        return $this->hasMany(Inventory::class);
+    }
+}
