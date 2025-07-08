be4cda0cf5e0e8fdf2363ca3702276b04064b2e1
Shivaram <shivaram1216@gmail.com>
2025-07-08 17:51:25 +0530

    Add new product api created and search api also created

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Inventory;
+use App\Models\Product;
 use App\Models\Sale_item;
+use App\Models\Shop_user;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
@@ -42,5 +47,73 @@ class ProductController extends Controller
                 'total' => $total
             ];
         });
+    }
+
+    public function productDetails() {
+        $user = Auth::user();
+
+        $shop_id = Shop_user::where('user_id', $user->id)->first()->shop_id;
+
+        $products = Product::where('shop_id', $shop_id)->with('inventory')->get();
+
+        return response()->json([
+            'status' => true,
+            'products' => $products
+        ]);
+    }
+
+    public function productCategory() {
+        $categories = Category::all();
+        return response()->json([
+            'status' => true,
+            'categories' => $categories
+        ]);
+    }
+
+    public function addProduct(Request $request) {
+
+        $user = Auth::user();
+
+        $shop_id = Shop_user::where('user_id', $user->id)->first()->shop_id;
+
+        $product = Product::create([
+            'shop_id' => $shop_id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'category_id' => $request->category_id,
+            'barcode' => $request->barcode ?? null
+        ]);
+
+        Inventory::create([
+            'shop_id' => $shop_id,
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+            'stock_qty' => $request->stock,
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Product added successfully',
+            'product' => $product
+        ]);
+    }
+
+    public function searchProducts(Request $request) {
+
+        $name = $request->query('name');
+
+        $user = Auth::user();
+        $shop_id = Shop_user::where('user_id', $user->id)->first()->shop_id;
+
+        $products = Product::where('shop_id', $shop_id)
+                            ->when($name, function ($q) use ($name) {$q->where('name', 'like', '%' . $name . '%');})
+                            ->with('inventory')
+                            ->get();
+
+        return response()->json([
+            'status' => true,
+            'products' => $products
+        ]);
     }
 }
