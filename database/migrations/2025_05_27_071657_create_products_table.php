91c84888a289bb541b692da2cff710b7897e8f99
Shivaram <shivaram1216@gmail.com>
2025-07-08 17:54:36 +0530

    Update column barcode is nullable

--- database/migrations/2025_05_27_071657_create_products_table.php
+++ database/migrations/2025_05_27_071657_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('quantity');
             $table->string('price');
-            $table->string('barcode');
+            $table->string('barcode')->nullable();
         });
     }
 
