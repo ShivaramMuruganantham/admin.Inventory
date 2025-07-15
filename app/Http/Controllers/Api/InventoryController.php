f99e24fe7963a34d8cae2a7a5cbbdf16a97fe8d5
Shivaram <shivaram1216@gmail.com>
2025-07-15 09:34:35 +0530

    Update inventory stock in controller

--- app/Http/Controllers/Api/InventoryController.php
+++ app/Http/Controllers/Api/InventoryController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Inventory;
+use App\Models\Product;
+use App\Models\Shop_user;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class InventoryController extends Controller
 {
-    //
+    public function updateStock(Request $request) {
+
+        $user = Auth::user();
+
+        $shop_id = Shop_user::where('user_id', $user->id)->first()->shop_id;
+
+        $product = Product::where('shop_id', $shop_id)->where('name', $request->name)->first();
+
+        $inventory = Inventory::where('shop_id', $shop_id)->where('product_id', $product->id)->first();
+
+        $inventory->update([
+            'stock_qty' => $inventory->stock_qty + $request->stock,
+            'notes' => $request->notes
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Stock updated successfully'
+        ]);
+    }
 }
