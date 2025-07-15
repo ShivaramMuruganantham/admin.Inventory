a65a20b6d09cf2f3a57e7e1caf8648abe4aa513c
Shivaram <shivaram1216@gmail.com>
2025-07-15 09:36:52 +0530

    Creating sale Items entery for every items

--- app/Http/Controllers/Api/SaleItemsController.php
+++ app/Http/Controllers/Api/SaleItemsController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sale;
+use App\Models\Sale_item;
+use App\Models\Shop_user;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SaleItemsController extends Controller
 {
-    
+    function salesEntery(Request $request) {
+        $user = Auth::user();
+        $shopId = Shop_user::where('user_id', $user->id)->first()->shop_id;
+        $sale = Sale::create([
+            'shop_id' => $shopId,
+            'user_id' => $user->id,
+            'total_amount' => $request->total ?? null
+        ]);
+
+        foreach($request->items as $item) {
+            Sale_item::create([
+                'sale_id' => $sale->id,
+                'product_id' => $item['product_id'],
+                'quantity' => $item['quantity'],
+                'price' => $item['quantity'] * $item['price']
+            ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'sale' => $sale
+        ]);
+    }
 }
